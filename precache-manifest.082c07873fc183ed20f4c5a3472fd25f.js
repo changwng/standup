8d39da11201c2cdf8bd1d48c936e8a29f80f5b8b
jangwoong <changwng@gmail.com>
2019-10-24 15:41:01 +0900

    Updates

---
+++ precache-manifest.082c07873fc183ed20f4c5a3472fd25f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756eea7e86e9d98f735581321264a3a1",
+    "url": "/standup/index.html"
+  },
+  {
+    "revision": "6f8c26687ad182329f69",
+    "url": "/standup/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "188b0fe44c71e8256144",
+    "url": "/standup/static/js/2.a040a78b.chunk.js"
+  },
+  {
+    "revision": "6f8c26687ad182329f69",
+    "url": "/standup/static/js/main.c7150177.chunk.js"
+  },
+  {
+    "revision": "15e3149e77cf643f7e5b",
+    "url": "/standup/static/js/runtime-main.654bab7c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/standup/static/media/logo.25bf045c.svg"
+  }
+]);
